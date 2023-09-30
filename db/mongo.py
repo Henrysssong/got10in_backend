a73c2c3cf2ef586a711636ea4661c6047e59cf97
Henry Song <feidong2002@berkeley.edu>
2023-09-29 19:27:43 -0700

    changed mongoDB whitelist

--- db/mongo.py
+++ db/mongo.py
@@ -1,13 +1,14 @@
 import motor.motor_asyncio
 import os
+import asyncio
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/got10in")
+FIXIE_URL = os.getenv("FIXIE_URL")
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI, io_loop=asyncio.get_event_loop(), proxy=FIXIE_URL)
 database = client.got10in
 
 subscriptions = database["subscriptions"]
-# db/mongo.py
 
 def add_subscriber(email: str) -> bool:
     """Add a new subscriber to the database."""
